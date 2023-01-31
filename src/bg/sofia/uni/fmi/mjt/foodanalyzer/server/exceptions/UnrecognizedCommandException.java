0e676ff72df27d59fb3857bbdbaa4c4801c7247c
tuechki <dtuechki@gmail.com>
2023-01-31 11:27:05 +0200

    Add appropriate exception when an unknown command is inserted.

---
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/UnrecognizedCommandException.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions;
+
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums.CommandName;
+
+public class UnrecognizedCommandException extends Exception {
+
+    public UnrecognizedCommandException(String command) {
+        super("The command " + command + "is not supported." + System.lineSeparator() +
+            "The supported commands up to this moment are: " + CommandName.GET_FOOD.getName()
+                                                             + CommandName.GET_FOOD_REPORT.getName()
+                                                             + CommandName.GET_FOOD_BY_BARCODE.getName());
+    }
+}
