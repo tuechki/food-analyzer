5ae3f885d917be62f6a1d79ccae3ec66ea322fc9
tuechki <dtuechki@gmail.com>
2023-02-02 09:50:51 +0200

    Add static method to retrieve Enum value by name.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/CommandName.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/CommandName.java
@@ -1,17 +1,31 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum CommandName {
     GET_FOOD("get-food"),
     GET_FOOD_REPORT("get-food-report"),
     GET_FOOD_BY_BARCODE("get-food-by-barcode");
+
+    private static final Map<String, CommandName> LOOKUP = new HashMap<>();
+
+    static {
+        for (CommandName commandName : values()) {
+            LOOKUP.put(commandName.getName(), commandName);
+        }
+    }
 
     private final String name;
     CommandName(String name) {
         this.name = name;
     }
 
-
     public String getName() {
         return this.name;
+    }
+
+    public static CommandName getByName(String name) {
+        return LOOKUP.get(name);
     }
 }
