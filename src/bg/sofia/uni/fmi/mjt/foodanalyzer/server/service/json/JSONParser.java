7c27fd5d99383e3f2be4bede7cdaafa872448126
tuechki <dtuechki@gmail.com>
2023-05-07 17:13:33 +0300

    Add json parser using Gson.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/service/json/JSONParser.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/service/json/JSONParser.java
@@ -1,28 +1,41 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.service;
+package bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.json;
 
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.dto.Food;
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.dto.FoodReport;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.List;
 
 public class JSONParser {
-    public static List<Food> toListOfFoods(String inputJson) {
+    public static List<Food> toListOfFoods(String foodsJson) {
         Type type = new TypeToken<List<Food>>() { }.getType();
-
         Gson gson = new GsonBuilder().registerTypeAdapter(type, new FoodDeserializer()).create();
 
-        return gson.fromJson(inputJson, type);
+        return gson.fromJson(foodsJson, type);
     }
 
-    public static FoodReport toFoodReport(String inputJson) {
+    public static FoodReport toFoodReport(String foodReportJson) {
         Gson gson = new Gson();
 
-        FoodReport foodReport = gson.fromJson(inputJson, FoodReport.class);
+        return gson.fromJson(foodReportJson, FoodReport.class);
+    }
 
-        return foodReport;
+    private static class FoodDeserializer implements JsonDeserializer<List<Food>> {
+
+        @Override
+        public List<Food> deserialize(JsonElement jsonElement, Type type,
+                                      JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            JsonElement content = jsonElement.getAsJsonObject().get("foods");
+            type = new TypeToken<List<Food>>() { }.getType();
+
+            return new Gson().fromJson(content, type);
+        }
     }
 }
