edb865dbd5742aeed66491bf0f22a745e09ed1f2
tuechki <dtuechki@gmail.com>
2023-05-07 16:48:49 +0300

    Add serial number.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/dto/FoodNutrient.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/dto/FoodNutrient.java
@@ -1,8 +1,11 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.server.dto;
 
+import java.io.Serial;
 import java.io.Serializable;
 
-public class FoodNutrient implements Serializable {
-    private Nutrient nutrient;
-    private String amount;
+public record FoodNutrient(Nutrient nutrient, String amount) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -6100149948476986141L;
+
 }
