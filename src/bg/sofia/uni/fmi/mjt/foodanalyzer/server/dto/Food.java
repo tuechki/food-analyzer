286760cf6075837ca7aff0320efdbc28b4f02369
tuechki <dtuechki@gmail.com>
2023-05-07 16:48:16 +0300

    Add serial number, equals and hashcode.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/dto/Food.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/dto/Food.java
@@ -1,13 +1,23 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.server.dto;
 
-public class Food {
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Food implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7610618888105857515L;
 
     private String description;
-
     private String fdcId;
-
     private String gtinUpc;
 
+    public Food(String description, String fdcId, String gtinUpc) {
+        this.description = description;
+        this.fdcId = fdcId;
+        this.gtinUpc = gtinUpc;
+    }
 
     public String getFdcId() {
         return fdcId;
@@ -15,5 +25,17 @@ public class Food {
 
     public String getGtinUpc() {
         return gtinUpc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Food food)) return false;
+        return getFdcId().equals(food.getFdcId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFdcId());
     }
 }
