98f4db339c23d2d36a25005ccac629f20df4f4f9
tuechki <dtuechki@gmail.com>
2023-05-07 16:37:29 +0300

    Add enums to better represent api fields.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/NutrientName.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/NutrientName.java
@@ -1,2 +1,19 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums;public enum Nutrient {
+package bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums;
+
+public enum NutrientName {
+    PROTEIN("Protein"),
+    LIPID("Total lipid (fat)"),
+    CARBOHYDRATE("Carbohydrate, by difference"),
+    FIBER("Fiber, total dietary"),
+    ENERGY("Energy");
+
+    private final String name;
+    NutrientName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/Unit.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/enums/Unit.java
@@ -1,2 +1,15 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums;public enum Unit {
+package bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums;
+public enum Unit {
+    GRAM("g"),
+    KCAL("kcal");
+
+    private final String name;
+    Unit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
