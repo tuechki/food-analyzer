fc761aec02780acf1ea71ea0b5cfe5bab4a7436b
tuechki <dtuechki@gmail.com>
2023-05-07 18:25:32 +0300

    Update Application start class.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/Application.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/Application.java
@@ -2,22 +2,22 @@ package bg.sofia.uni.fmi.mjt.foodanalyzer;
 
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.FoodAnalyzerServer;
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.controller.FDCClient;
-import bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.CacheInMemoryStorage;
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.cache.FoodDataStorage;
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.cache.FoodDataCache;
 
 import java.net.http.HttpClient;
 
 public class Application {
 
-    private static final int PORT = 7777;
+    private static final int PORT = 6666;
 
     public static void main(String[] args) {
-        CacheInMemoryStorage cacheInMemoryStorage = new CacheInMemoryStorage();
+        FoodDataCache cacheStorage = new FoodDataStorage();
         FDCClient fdcClient = new FDCClient(HttpClient.newBuilder().build());
 
-        FoodAnalyzerServer server = new FoodAnalyzerServer(PORT, cacheInMemoryStorage, fdcClient);
+        FoodAnalyzerServer server = new FoodAnalyzerServer(PORT, cacheStorage, fdcClient);
 
         server.start();
-
 
     }
 }
