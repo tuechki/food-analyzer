21cfaf0bc0f35fb6b076fd364be0d8aa1c5bc29a
tuechki <dtuechki@gmail.com>
2023-05-07 16:43:25 +0300

    Return previous state of Command interface.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/command/Command.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/command/Command.java
@@ -1,26 +1,25 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.commands;
+package bg.sofia.uni.fmi.mjt.foodanalyzer.server.command;
 
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.controller.FDCClient;
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums.CommandName;
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.TooFewArgumentsException;
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.TooManyArgumentsException;
-import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.UnrecognizedCommandException;
-import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.WrongArgumentsException;
-import bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.cache.CacheStorage;
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.UnsupportedCommandException;
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions.WrongBarcodeArgumentsException;
+import bg.sofia.uni.fmi.mjt.foodanalyzer.server.service.cache.FoodDataCache;
 
 public interface Command {
-
-    String execute(CacheStorage cacheStorage, FDCClient fdcClient) throws WrongArgumentsException;
+    String execute(FoodDataCache cacheStorage, FDCClient fdcClient) throws WrongBarcodeArgumentsException;
 
     static Command of(String command)
-        throws UnrecognizedCommandException, TooFewArgumentsException, TooManyArgumentsException {
+        throws UnsupportedCommandException, TooFewArgumentsException, TooManyArgumentsException {
         String userCommand = command.split("\s+")[0];
         CommandName commandName = CommandName.getByName(userCommand);
         return switch (commandName) {
             case GET_FOOD -> new GetFood(command);
             case GET_FOOD_REPORT -> new GetFoodReport(command);
             case GET_FOOD_BY_BARCODE -> new GetFoodByBarcode(command);
-            case null -> throw new UnrecognizedCommandException(userCommand);
+            case null -> throw new UnsupportedCommandException(userCommand);
         };
     }
 }
