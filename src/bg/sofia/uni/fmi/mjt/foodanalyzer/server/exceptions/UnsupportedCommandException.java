13582f57313ee38ccd0b02dbfa32f00c02613c1a
tuechki <dtuechki@gmail.com>
2023-05-07 16:57:08 +0300

    Update exception names.

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/UnsupportedCommandException.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/UnsupportedCommandException.java
@@ -2,9 +2,9 @@ package bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions;
 
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums.CommandName;
 
-public class UnrecognizedCommandException extends Exception {
+public class UnsupportedCommandException extends Exception {
 
-    public UnrecognizedCommandException(String command) {
+    public UnsupportedCommandException(String command) {
         super("The command " + command + "is not supported." + System.lineSeparator() +
             "The supported commands up to this moment are: " + CommandName.GET_FOOD.getName() + ", "
                                                              + CommandName.GET_FOOD_REPORT.getName() + ", "
--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/WrongBarcodeArgumentsException.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/WrongBarcodeArgumentsException.java
@@ -2,8 +2,8 @@ package bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions;
 
 import bg.sofia.uni.fmi.mjt.foodanalyzer.server.enums.ArgumentFlag;
 
-public class WrongArgumentsException extends Exception {
-    public WrongArgumentsException() {
+public class WrongBarcodeArgumentsException extends Exception {
+    public WrongBarcodeArgumentsException() {
         super("Wrong arguments. Expected argument " + ArgumentFlag.CODE + " or" + ArgumentFlag.IMG + ".");
     }
 }
